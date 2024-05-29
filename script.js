b328e776fb9d1860c0c0a2a12041a7c694ebe585
mohammed kaif <mkaif555123@gmail.com>
2024-05-29 16:03:59 +0500

    sweet alert practice 50% done'

--- script.js
+++ script.js
@@ -2,5 +2,21 @@ let button = document.getElementById("btn");
 
 button.addEventListener("click" , (event) =>{
    event.preventDefault();
-   console.log("kaif");
+   //console.log("kaif");
+   let age = +prompt("enter your age")
+   if(age >=30){
+    swal({
+        title:"OOPS !...",
+        text:"You Are Not Allow In This Course",
+        icon:"error"
+       })
+   }
+   else{
+    swal({
+        title:"THANK YOU...",
+        text:"You Are  Allow In This Course",
+        icon:"success"
+       })
+   }
+  
 })
