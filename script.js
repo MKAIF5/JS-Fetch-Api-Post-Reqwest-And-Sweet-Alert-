131de7e109bbe4996c113fcc7c08ecbf9a80d37c
mohammed kaif <mkaif555123@gmail.com>
2024-05-31 15:05:50 +0500

    sweet alert done 100%

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-let button = document.getElementById("btn");
+// let button = document.getElementById("btn");
 
-button.addEventListener("click" , (event) =>{
-   event.preventDefault();
-   //console.log("kaif");
-   let age = +prompt("enter your age")
-   if(age >=30){
-    swal({
-        title:"OOPS !...",
-        text:"You Are Not Allow In This Course",
-        icon:"error"
-       })
-   }
-   else{
-    swal({
-        title:"THANK YOU...",
-        text:"You Are  Allow In This Course",
-        icon:"success"
-       })
-   }
+// button.addEventListener("click" , (event) =>{
+//    event.preventDefault();
+//    //console.log("kaif");
+//    let age = +prompt("enter your age")
+//    if(age >=30){
+//     swal({
+//         title:"OOPS !...",
+//         text:"You Are Not Allow In This Course",
+//         icon:"error",
+//         button:"Back"
+//        })
+//    }
+//    else{
+//     swal({
+//         title:"THANK YOU...",
+//         text:"You Are  Allow In This Course",
+//         icon:"success",
+//         button:"Done"
+//        })
+//    }
   
-})
+// })
+
